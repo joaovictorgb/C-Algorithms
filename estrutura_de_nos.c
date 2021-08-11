fba3f33ea2aecc9d0b9077d7f1d7104280c06cf1
João Victor Barbosa <88726494+joaovictorgb@users.noreply.github.com>
2021-08-11 09:36:48 -0300

    Create estrutura_de_nos.c

---
+++ estrutura_de_nos.c
@@ -0,0 +1,13 @@
+// Crie a estrutura de um nó para uma lista encadeada.
+#include<stdio.h>
+Struct no {
+        float dados
+        Struct no * proximo
+};
+//Crie a estrutura de um nó para uma lista duplamente encadeada.
+#include<stdio.h>
+Struct no {
+        float dados
+        Struct no *anterior
+        struct no * proximo
+};
